4ab8e9057f7f115b1280227a8c56064f77e580c3
Ujwal1830 <yangalwarujwal1830@gmail.com>
2024-12-22 05:56:30 +0530

    removed the @ts-ignore

--- components/ui/icon-cloud.tsx
+++ components/ui/icon-cloud.tsx
@@ -80,7 +80,7 @@ export default function IconCloud({ iconSlugs }: DynamicCloudProps) {
   }, [data, theme]);
 
   return (
-    // @ts-ignore
+    // @ts-nocheck
     <Cloud {...cloudProps}>
       <>{renderedIcons}</>
     </Cloud>
